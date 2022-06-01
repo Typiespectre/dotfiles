8496885ebf4fe6e3cf685b54abda2b39b940622b
Choi GyeongGeon <ckgun@naver.com>
2022-06-01 16:07:12 +0900

    20220601 Typiespectre [modified] symbolic link to original file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,1 +1,14 @@
-/Users/ckgun/.hammerspoon/init.lua
+-- https://johngrib.github.io/wiki/hammerspoon/
+-- https://www.hammerspoon.org/go/
+
+-- functions for open application
+function openEditor()
+	hs.application.launchOrFocus('CotEditor')
+end
+
+function openTerm()
+	hs.application.launchOrFocus('iTerm')
+end
+
+hs.hotkey.bind({'ctrl'}, 't', openTerm)
+hs.hotkey.bind({'ctrl'}, 'e', openEditor)
